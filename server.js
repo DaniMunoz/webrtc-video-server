85ea6825c83e254e3009648107af918f84e192c3
Dani <danielmunozgil@gmail.com>
2023-04-30 20:25:56 +0200

    added new domain to serve connections to

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-const server = require('http').Server(app)
+const server = require("http").Server(app);
 
-const { v4: uuidV4} = require('uuid')
+const { v4: uuidV4 } = require("uuid");
 
 //var morgan = require('morgan')
 //app.use(morgan('combined'))
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.use(cors());
-//origin: ['http://localhost:3000', 'http://localhost:8000', 'https://webrtc-video-client.vercel.app']
+//origin: 'https://webrtc-video-client.vercel.app',
 var corsOptions = {
-    origin: 'https://webrtc-video-client.vercel.app',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  origin: [
+    "https://videochat-danimunoz.vercel.app/",
+    "http://127.0.0.1:4173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "https://webrtc-video-client.vercel.app",
+  ],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-//origin: ['http://localhost:3000', 'http://localhost:8000', 'https://webrtc-video-client.vercel.app']
-const io = require('socket.io')(server, {
-    cors: {
-        origin: 'https://webrtc-video-client.vercel.app',
-        /*
+//origin: 'https://webrtc-video-client.vercel.app',
+const io = require("socket.io")(server, {
+  cors: {
+    origin: [
+      "https://videochat-danimunoz.vercel.app/",
+      "http://127.0.0.1:4173",
+      "http://127.0.0.1:5173",
+      "http://localhost:3000",
+      "http://localhost:8000",
+      "https://webrtc-video-client.vercel.app",
+    ],
+    /*
         handlePreflightRequest: (req, res) => {
             res.writeHead(200, {
                 "Access-Control-Allow-Origin": "*",
@@ -33,35 +47,36 @@ const io = require('socket.io')(server, {
             })
         }
         */
-    }
+  },
 });
 
 app.use(express.json());
 
-app.get('/message', (req, res) => {
-    res.json({ message: "Hello from server!" });
+app.get("/message", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
-app.get('/room', cors(corsOptions), (req, res) => {
-    //res.redirect(`/room/${uuidV4()}`)
-    const room = uuidV4();
-    res.json({ room: room });
-})
+app.get("/room", cors(corsOptions), (req, res) => {
+  //res.redirect(`/room/${uuidV4()}`)
+  const room = uuidV4();
+  res.json({ room: room });
+});
 
-app.get('/room/:room', cors(corsOptions), (req, res) => {
-    //res.render('room', {roomId: req.params.room})
-})
+app.get("/room/:room", cors(corsOptions), (req, res) => {
+  //res.render('room', {roomId: req.params.room})
+});
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).emit('user-connected', userId)
-        socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', userId)
-        })
-    })
-})
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server is running`);
+server.listen(8000, () => {
+  //server.listen(process.env.PORT, () => {
+  console.log(`Server is running`);
 });
